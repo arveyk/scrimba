e655b3ac262166dc59b1771831e9cccbc545d927
Harvey W. Kis <harveykisiangani@gmail.com>
2024-11-04 21:12:05 +0300

    Creating a person type object

--- typescript/2-index.ts
+++ typescript/2-index.ts
@@ -9,4 +9,31 @@ let person1 = {
   name: "John",
   age: 42,
   isStudent: true
+  address: {
+    streer: string,
+    city: string,
+    country: string
+  }
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
 }
