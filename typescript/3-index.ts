1237308376c696924f11e1eccf13a09dff11359b
Harvey W. Kis <harveykisiangani@gmail.com>
2024-11-04 21:38:10 +0300

    Typing arrays

---
+++ typescript/3-index.ts
@@ -0,0 +1,28 @@
+//Typing array
+let age: number[] = [100, 4, 34];
+
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+}
+
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true,
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false,
+}
+
+/**
+ * Challenge: create an array of people objects and
+ * manually type it as an array of Person types
+ */
+
+let people: Person[] = [person1, person2]
+
